ea8b71a16370b81a494c15b4c513755ba02c1fcd
jarwonozt <jarwonoztaveiro@gmail.com>
2024-06-15 15:47:12 +0700

    chore: initial commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/messages', [App\Http\Controllers\HomeController::class, 'messages'])
+    ->name('messages');
+Route::post('/message', [App\Http\Controllers\HomeController::class, 'message'])
+    ->name('message');
